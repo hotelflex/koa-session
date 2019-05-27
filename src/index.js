4a5217212e6adba4fff442dfe5dad1f907bed7ef
Rich Turnbull <rpturnbull1@gmail.com>
2019-05-27 15:45:59 -0700

    fixing errors

--- src/index.js
+++ src/index.js
@@ -5,21 +5,19 @@ module.exports = authUrl => async (ctx, next) => {
   if (!ctx.request.token) {
     ctx.request.session = {}
   } else {
-    try {
-      const resp = await fetch(authUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ token: ctx.request.token }),
-      })
-      if (resp.status !== 200) throw new Error()
-      ctx.request.session = await resp.json()
-    } catch (err) {
-      throw Boom.badGateway(
-        `Authenticator ${authUrl} failed to resolve the session: ${
-          err.message
-        }`,
-      )
-    }
+
+    const resp = await fetch(authUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ token: ctx.request.token }),
+    })
+    
+    if(resp.status === 401) 
+      throw Boom.unauthorized()
+    if(resp.status !== 200) 
+      throw Boom.badGateway(`Authenticator ${authUrl} failed to resolve the session.`)
+
+    ctx.request.session = await resp.json()
   }
 
   return next()
