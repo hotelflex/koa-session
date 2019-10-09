018852f2a4d21fa630224f264bfc59103e5476b5
Rich Turnbull <rpturnbull1@gmail.com>
2019-10-09 13:45:47 +0100

    fixed 401 errors

--- src/index.js
+++ src/index.js
@@ -5,19 +5,25 @@ module.exports = authUrl => async (ctx, next) => {
   if (!ctx.request.token) {
     ctx.request.session = {}
   } else {
-
-    const resp = await fetch(authUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ token: ctx.request.token }),
-    })
-    
-    if(resp.status === 401) 
-      throw Boom.unauthorized()
-    if(resp.status !== 200) 
-      throw Boom.badGateway(`Authenticator ${authUrl} failed to resolve the session.`)
-
-    ctx.request.session = await resp.json()
+    try {
+      const resp = await fetch(authUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ token: ctx.request.token }),
+      })
+      if(resp.status === 200) {
+        ctx.request.session = await resp.json();
+      }
+      else if(resp.status === 401) {
+        //ignore
+      } 
+      else {
+        throw new Error(resp.statusText);
+      }
+    } catch (err) {
+      console.log(err);
+      throw Boom.badGateway(`${authUrl} failed to resolve the session.`)
+    }
   }
 
   return next()
